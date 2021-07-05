4668127b9241be3dbbeb638dc7b0f773145e762c
XiaoLiSean <hsiao.li.sean@gmail.com>
2021-07-04 22:25:11 -0400

    linear least-sq calibration added

--- experiment/calibration.py
+++ experiment/calibration.py
@@ -92,38 +92,49 @@ def calibration(filename):
         # append useful data to dataSynchronized for later calibration
         MarkerXs = np.multiply(markerMeasure['r']/1000, np.cos(np.deg2rad(markerMeasure['a'])))
         MarkerYs = np.multiply(markerMeasure['r']/1000, np.sin(np.deg2rad(markerMeasure['a'])))
-        if len(dataSynchronized) == 0:
-            if len(MarkerXs) != 1:
-                print("First lidar measurement got multiple dots: Xs {}; Ys {}".format(MarkerXs, MarkerYs))
-                exit(0)
-            dataSynchronized.append([groundTrue[0], groundTrue[1], MarkerXs[0], MarkerYs[0]])
-        else:
-            # filter out ambiguous lidar measurement
-            for i in range(len(MarkerXs)):
-                drsq_lidar = (MarkerXs[i]-dataSynchronized[-1][2])**2 + (MarkerYs[i]-dataSynchronized[-1][3])**2
-                drsq_opti = (groundTrue[0]-dataSynchronized[-1][2])**2 + (groundTrue[1]-dataSynchronized[-1][3])**2
-                if abs(drsq_lidar-drsq_opti)/drsq_opti <= DisplacementErrorPercentage:
-                    dataSynchronized.append([groundTrue[0], groundTrue[1], MarkerXs[i], MarkerYs[i]])
+        for i in range(len(MarkerXs)):
+            dataSynchronized.append([groundTrue[0], groundTrue[1], MarkerXs[i], MarkerYs[i]])
+    # --------------------------------------------------------------------------
+    # calibration calculation main: variables are [cos(t), sin(t), dx, dy]
+    # where the lidar measurement (x2, y2) and global pose (x1, y1) by OptiTrack
+    # can be formulated as:
+    # x1 = x2*cos(t) - y2*sin(t) + dx;
+    # y1 = x2*sin(t) + y2*cos(t) + dy;
+    A = []
+    b = []
+    for data in dataSynchronized:
+        x1 = data[0]
+        y1 = data[1]
+        x2 = data[2]
+        y2 = data[3]
+        A.append([x2, -y2, 1, 0])
+        b.append([x1])
+        A.append([y2, x2, 0, 1])
+        b.append([y1])
+    A = np.array(A)
+    b = np.array(b)
+    lidar_pose = np.linalg.lstsq(A, b)[0]
+    theta = np.arctan2(lidar_pose[1], lidar_pose[0])
+    dx = lidar_pose[2]
+    dy = lidar_pose[3]
+    print("Calibration result: [dx, dy, theta]=({}[m],{}[m],{}[deg])".format(dx, dy, np.rad2deg(theta)))
     # --------------------------------------------------------------------------
     # Loop through data for visual check
     ax = plt.subplot(111)
-    print(dataSynchronized)
     for z in dataSynchronized:
         ax.clear()
         plt.plot(z[0], z[1], 'ro', markersize=3)
-        plt.plot(z[2], z[3], 'bx', markersize=1)
+        plt.plot(z[2]*np.cos(theta)-z[3]*np.sin(theta)+dx, z[2]*np.sin(theta)+z[3]*np.cos(theta)+dy, 'bx', markersize=1)
         ax.grid(True)
         ax.axis('equal')
         ax.set_xlim([-5, 5])
         ax.set_ylim([-5, 5])
-        plt.pause(0.1)
+        plt.pause(0.01)
     plt.show()
-    # --------------------------------------------------------------------------
-    # calibration calculation main
 
 # ------------------------------------------------------------------------------
 if __name__ == '__main__':
     filename = 'calibrateRaw.npy'
-    # calibration(filename)
-    replayData(filename)
     # recordData(filename)
+    # replayData(filename)
+    calibration(filename)
