d2c35668cca53b1d9c3974ecbf358c2712937f12
XiaoLiSean <hsiao.li.sean@gmail.com>
2021-09-10 19:57:05 -0400

    new calibration date

--- experiment/params.py
+++ experiment/params.py
@@ -3,7 +3,7 @@
 # ==============================================================================
 # optitrack communication ip (win10 is the server, the ubuntu receiving data is client)
 # ==============================================================================
-ip_win10 = '192.168.1.2'
+ip_win10 = '192.168.1.5'
 ip_ubuntu_pc = '192.168.1.3'
 # ==============================================================================
 # Local lidar ports
@@ -18,8 +18,9 @@ LIDAR_PORTs = [LIDAR_PORT1, LIDAR_PORT2, LIDAR_PORT3]
 # ==============================================================================
 # [t1, t2], rmax: car is in the lidar field of view [t1,t2] within range of rmax
 # where the lidar measurements is in [0, 360 deg]
-CarToLidar1FoV = dict(FoVs=[[0,60], [300, 360]], rmax=1400)
-CarToLidar2FoV = dict(FoVs=[[0,60], [300, 360]], rmax=1400)
-CarToLidar3FoV = dict(FoVs=[[0,60], [300, 360]], rmax=1400)
+CarToLidar1FoV = dict(FoVs=[[0,70], [290, 360]], rmax=1000)
+CarToLidar2FoV = dict(FoVs=[[0,70], [290, 360]], rmax=1000)
+CarToLidar3FoV = dict(FoVs=[[0,70], [290, 360]], rmax=1000)
 CarToLidarFoVs = [CarToLidar1FoV, CarToLidar2FoV, CarToLidar3FoV]
-DistanceThreshold = [[0.18,0.2], [0.76,0.78], [0.72,0.74]] #range of l2 error between lidar and optitrack estimation in [meter]
+DistanceThreshold = [[0.45, 0.55], [0.02,0.12], [0.45, 0.55]] #range of l2 error between lidar and optitrack estimation in [meter]
+AngleThreshold = [5, 5, 5] #range of bearing error in degree between lidar and optitrack estimation in [meter]
